1be60f9f0ac0693bed8c8360ab5b54c1165831de
JTCCRAFT <jctcraft@gmail.com>
2026-09-12 22:28:06 -0500

    Add shapes menu and upgrade tier to idk.js

--- server/lib/definitions/entityAddons/idk/idk.js
+++ server/lib/definitions/entityAddons/idk/idk.js
@@ -1,3 +1,6 @@
+const {base} = require('../../constants.js');
+const {makeMenu} = require("../../facilitators");
+//idk
 Class.shapesMenu = makeMenu("Shapes Menu")
 Class.shapesMenu.UPGRADES_TIER_0 = ["abomination"]
 Class.menu_addons.UPGRADES_TIER_0.push("shapesMenu");
