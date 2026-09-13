0f2ddc70b27f1e2f886d3c00ea8632167855be69
JTCCRAFT <jctcraft@gmail.com>
2026-09-12 22:30:22 -0500

    Change upgrade from 'abomination' to 'egg'

--- server/lib/definitions/entityAddons/idk/idk.js
+++ server/lib/definitions/entityAddons/idk/idk.js
@@ -2,5 +2,5 @@ const {base} = require('../../constants.js');
 const {makeMenu} = require("../../facilitators");
 //idk
 Class.shapesMenu = makeMenu("Shapes Menu")
-Class.shapesMenu.UPGRADES_TIER_0 = ["abomination"]
+Class.shapesMenu.UPGRADES_TIER_0 = ["egg"]
 Class.menu_addons.UPGRADES_TIER_0.push("shapesMenu");
